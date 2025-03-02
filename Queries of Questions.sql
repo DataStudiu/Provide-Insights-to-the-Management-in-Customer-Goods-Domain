3969831ca444ee2b61ebea9ce9f74c28839c7db7
Muhammad Muneeb Nasir <129332483+DataStudiu@users.noreply.github.com>
2025-03-02 23:39:51 +0500

    Update Queries of Questions.sql

--- Queries of Questions.sql
+++ Queries of Questions.sql
@@ -1,8 +1,7 @@
 SELECT * FROM dim_customer;
 
 -- QUESTION NO  : 1
--- Provide the list of markets in which customer "Atliq Exclusive" operates its
--- business in the APAC region.
+-- Provide the list of markets in which customer "Atliq Exclusive" operates its business in the APAC region.
 
 select
 distinct market , customer  , region 
@@ -11,11 +10,7 @@ where
 customer = "Atliq Exclusive" and region = "APAC" ;
 
 -- Question 2 :
-  -- What is the percentage of unique product increase in 2021 vs. 2020? The
--- final output contains these fields,
--- unique_products_2020
--- unique_products_2021
--- percentage_chg 
+  -- What is the percentage of unique product increase in 2021 vs. 2020?
 
 
 WITH unique_2020 AS (
@@ -36,10 +31,7 @@ FROM unique_2020, unique_2021;
 
 -- Question no 3 :
 --  Provide a report with all the unique product counts for each segment and
--- sort them in descending order of product counts. The final output contains
--- 2 fields,
--- segment
--- product_count
+-- sort them in descending order of product counts.
 
 select 
 segment ,
@@ -51,12 +43,8 @@ segment ,
  
 -- Question 4 : 
 -- Follow-up: Which segment had the most increase in unique products in
--- 2021 vs 2020? The final output contains these fields,
--- segment
--- product_count_2020
--- product_count_2021
--- difference
- 
+-- 2021 vs 2020? 
+
  with segment_count_2020 as (
 select 
  segment,
@@ -89,10 +77,6 @@ select
  
 -- Question no 5  :
 --  Get the products that have the highest and lowest manufacturing costs.
--- The final output should contain these fields,
--- product_code
--- product
--- manufacturing_cost
 
 with high_cost_product as
 (
@@ -126,10 +110,7 @@ select
 -- Question no 6 :
 --  Generate a report which contains the top 5 customers who received an
 -- average high pre_invoice_discount_pct for the fiscal year 2021 and in the
--- Indian market. The final output contains these fields,
--- customer_code
--- customer
--- average_discount_percentage
+-- Indian market. 
 
 select 
  fact_pre_invoice_deductions.customer_code,
@@ -149,10 +130,7 @@ select
 -- Get the complete report of the Gross sales amount for the customer “Atliq
 -- Exclusive” for each month. This analysis helps to get an idea of low and
 -- high-performing months and take strategic decisions.
--- The final report contains these columns:
--- Month
--- Year
--- Gross sales Amount
+
 
 select
  month(date) as month ,
@@ -184,10 +162,7 @@ limit 1 ;
 
 -- Question no : 9
 -- Which channel helped to bring more gross sales in the fiscal year 2021
--- and the percentage of contribution? The final output contains these fields,
--- channel
--- gross_sales_mln
--- percentage
+-- and the percentage of contribution?
 
 WITH sales AS(
 SELECT channel,
@@ -208,14 +183,7 @@ CROSS JOIN (SELECT sum(gross_sales_mln) AS total FROM sales) t;
 
 -- Question no :10
 -- Get the Top 3 products in each division that have a high
--- total_sold_quantity in the fiscal_year 2021? The final output contains these
--- fields,
--- division
--- product_code
--- codebasics.io
--- product
--- total_sold_quantity
--- rank_order
+-- total_sold_quantity in the fiscal_year 2021?
 
 with Results as (
 select division ,
